7dcfc9ce3dab098273d8e54cd1783a0fe0601458
NickMonro <nazarovdelal@gmail.com>
2018-03-17 22:46:45 +0300

    first commit

---
+++ js/popup_img.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function() { 
+	
+	$(".myImg").click(function(){	
+	  	var img = $(this);	
+		var src = img.attr('value'); 
+		var alt = img.attr('alt');
+		$("body").append("<div class='popup'>"+ 
+						 "<div class='popup_bg'></div>"+ 
+						 "<img src="+src+" class='popup_img' />"+"<p class='resp'>"+alt+"</p>"+ 
+						 "</div>"); 
+		$(".popup").fadeIn(800); 
+		$(".popup_bg").click(function(){		   
+			$(".popup").fadeOut(800);	
+			setTimeout(function() {	
+			  $(".popup").remove();
+			}, 800);
+		});
+	});
+ 
+});
